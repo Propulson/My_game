c5ca78c7fe69950a9dece2de6ea5bf519d0f76ac
Propulson <158391413+Propulson@users.noreply.github.com>
2024-05-20 00:03:14 +1200

    Create color for game

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,17 +5,18 @@ class AlienInvasion:
     """class for resource and rule game"""
 
     def __init__(self):
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((1920, 1080))
         pygame.display.set_caption('Alien Invasion')
+        self.bg_color = (230, 230, 230)  # Color
 
-    @staticmethod
-    def run_game():
+    def run_game(self):
         """Main game start"""
         while True:
             """checking mouse and keyboard"""
             for even in pygame.event.get():
                 if even.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
 
 
