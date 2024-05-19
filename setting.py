9298094abcdc33444a81e0862a349e0e45b75e4b
Propulson <158391413+Propulson@users.noreply.github.com>
2024-05-20 00:02:55 +1200

    Create settings

---
+++ setting.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Class for saving everything games settings"""
+    def __init__(self):
+        """initialization game settings"""
+        # Screen settings
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color = (230, 230, 230)
