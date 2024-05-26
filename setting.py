9f1182b0442a4c9f749662d5576decfe53d506a4
Propulson <158391413+Propulson@users.noreply.github.com>
2024-05-26 20:58:02 +1200

    include bullet

--- setting.py
+++ setting.py
@@ -3,6 +3,10 @@ class Settings():
     def __init__(self):
         """initialization game settings"""
         # Screen settings
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
         self.screen_width = 1920
         self.screen_height = 1080
         self.bg_color = (230, 230, 230)
