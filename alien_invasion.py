1ede4542b9b024d89db1c94cb23d1745cc7d3b77
Propulson <158391413+Propulson@users.noreply.github.com>
2024-05-26 21:38:31 +1200

    delete missiles

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from setting import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -17,13 +18,20 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height)
         )
         pygame.display.set_caption('Alien Invasion')
-        self.ship = Ship(screen)
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Main game start"""
         while True:
             """checking mouse and keyboard"""
             self._check_events()
+            self.ship.update()
+            self.bullets.update()
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
             self._update_screen()
 
     def _check_events(self):
@@ -43,6 +51,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif even.key == pygame.K_q:
             sys.exit()
+        elif even.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, even):
         if even.key == pygame.K_RIGHT:
@@ -50,11 +60,19 @@ class AlienInvasion:
         elif even.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create new missile"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """refresh images on view"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-        pygame.display.flip()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+    pygame.display.flip()
 
 
 if __name__ == '__main__':
