388feac5f6f2c90125f4b14f2f1faa721e16cbec
Propulson <158391413+Propulson@users.noreply.github.com>
2024-05-26 21:51:39 +1200

    settings for missile

--- setting.py
+++ setting.py
@@ -7,6 +7,7 @@ class Settings():
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 3
         self.screen_width = 1920
         self.screen_height = 1080
         self.bg_color = (230, 230, 230)
