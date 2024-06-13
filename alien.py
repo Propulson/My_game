b55f73a5ec8d3f75451282802a427b11f11b5af9
Propulson <158391413+Propulson@users.noreply.github.com>
2024-06-13 22:15:08 +1200

    finish

--- alien.py
+++ alien.py
@@ -18,6 +18,12 @@ class Alien(Sprite):
 
         self.x = float(self.rect.x)
 
+    def check_edges(self):
+        """true if alien right pos"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
     def update(self):
         """move right alien"""
         self.x += self.settings.alien_speed
@@ -25,10 +31,3 @@ class Alien(Sprite):
         self.x += (self.settings.alien_speed *
                    self.settings.fleet_direction)
         self.rect.x = self.x
-
-    def check_edges(self):
-        """true if alien right pos"""
-        screen_rect = self.screen.get_rect()
-        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
-            return True
-
--- game_stats.py
+++ game_stats.py
@@ -5,8 +5,10 @@ class GameStats():
         self.settings = ai_game.settings
         self.reset_stats()
         self.game_active = False
+        self.high_score = 0
 
     def reset_stats(self):
         """resets stats to default values"""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
